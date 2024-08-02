7e5e11bd10028c70f2f42e409d658a625393fd90
CanftIn <wwc7033@gmail.com>
2024-08-02 14:02:59 +0800

    update ilist_base

--- include/llcl/Standard/ADT/ilist_base.h
+++ include/llcl/Standard/ADT/ilist_base.h
@@ -19,6 +19,67 @@ class ilist_base {
     Prev.setNext(&N);
     Next.setPrev(&N);
   }
+
+  static void removeImpl(node_base_type& N) {
+    node_base_type* Prev = N.getPrev();
+    node_base_type* Next = N.getNext();
+    Next->setPrev(Prev);
+    Prev->setNext(Next);
+    N.setPrev(nullptr);
+    N.setNext(nullptr);
+  }
+
+  static void removeRangeImpl(node_base_type& First, node_base_type& Last) {
+    node_base_type* Prev = First.getPrev();
+    node_base_type* Final = Last.getPrev();
+    Last.setPrev(Prev);
+    Prev->setNext(&Last);
+    First.setPrev(nullptr);
+    Final->setNext(nullptr);
+  }
+
+  static void transferBeforeImpl(node_base_type& Next, node_base_type& First,
+                                 node_base_type& Last) {
+    if (&Next == &Last || &First == &Last)
+      return;
+
+    // Position cannot be contained in the range to be transferred.
+    assert(&Next != &First &&
+           // Check for the most common mistake.
+           "Insertion point can't be one of the transferred nodes");
+
+    node_base_type& Final = *Last.getPrev();
+
+    // Detach from old list/position.
+    First.getPrev()->setNext(&Last);
+    Last.setPrev(First.getPrev());
+
+    // Splice [First, Final] into its new list/position.
+    node_base_type& Prev = *Next.getPrev();
+    Final.setNext(&Next);
+    First.setPrev(&Prev);
+    Prev.setNext(&First);
+    Next.setPrev(&Final);
+  }
+
+  template <class T>
+  static void insertBefore(T& Next, T& N) {
+    insertBeforeImpl(Next, N);
+  }
+
+  template <class T>
+  static void remove(T& N) {
+    removeImpl(N);
+  }
+  template <class T>
+  static void removeRange(T& First, T& Last) {
+    removeRangeImpl(First, Last);
+  }
+
+  template <class T>
+  static void transferBefore(T& Next, T& First, T& Last) {
+    transferBeforeImpl(Next, First, Last);
+  }
 };
 
 }  // namespace llcl
